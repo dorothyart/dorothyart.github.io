c5a13cdfc73c46cf608b57950273917789013459
dorothyart <129077502+dorothyart@users.noreply.github.com>
2023-03-31 22:53:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let display = document.getElementById('display');
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-function addToDisplay(value) {
-   display.value += value;
-}
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let brushSize = 10;
+let eraserSize = 10;
+let currentTool = "pencil";
 
-function clearDisplay() {
-   display.value = '';
-}
+// Set canvas size to match parent container
+canvas.width = canvas.parentNode.clientWidth;
+canvas.height = canvas.parentNode.clientHeight;
 
-function calculate() {
-   try {
-      display.value = eval(display.value);
-   } catch (error) {
-      display.value = 'Error';
-   }
-}
+// Handle window resize to update canvas size
+window.addEventListener("resize", () => {
+  canvas.width = canvas.parentNode.clientWidth;
+  canvas.height = canvas.parentNode.clientHeight;
+});
 
+// Set up event listeners for drawing
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseout", stopDrawing);
+
+// Handle tool selection
+const toolButtons = document.querySelectorAll("#toolbar button");
+toolButtons.forEach((button

5e73a79065f2a1953a055fc4186660528c2d2ec8
dorothyart <129077502+dorothyart@users.noreply.github.com>
2023-03-31 23:13:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-let hue = 0;
-let brushSize = 10;
-let eraserSize = 10;
-let currentTool = "pencil";
 
-// Set canvas size to match parent container
-canvas.width = canvas.parentNode.clientWidth;
-canvas.height = canvas.parentNode.clientHeight;
-
-// Handle window resize to update canvas size
-window.addEventListener("resize", () => {
-  canvas.width = canvas.parentNode.clientWidth;
-  canvas.height = canvas.parentNode.clientHeight;
+canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+    [lastX, lastY] = [event.offsetX, event.offsetY];
 });
 
-// Set up event listeners for drawing
-canvas.addEventListener("mousedown", startDrawing);
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", stopDrawing);
-canvas.addEventListener("mouseout", stopDrawing);
+canvas.addEventListener('mousemove', (event) => {
+    if (isDrawing) {
+        const currentX = event.offsetX;
+        const currentY = event.offsetY;
 
-// Handle tool selection
-const toolButtons = document.querySelectorAll("#toolbar button");
-toolButtons.forEach((button
+        context.beginPath();
+        context.moveTo(lastX, lastY);
+        context.lineTo(currentX, currentY);
+        context.stroke();
+
+        [lastX, lastY] = [currentX, currentY];
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+canvas.addEventListener('mouseout', () => {
+    isDrawing = false;
+});
